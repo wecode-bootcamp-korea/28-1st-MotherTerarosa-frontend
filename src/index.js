fe12aa49c6afbf5d870c8241629ca07c18c96b80
YunKukPark <a01043340999@gmail.com>
2021-12-27 18:33:56 +0900

    Add: reset.scss, 기본 폰트 적용 - index.html googlefont CDN 추가 - index.js 에 reset.scss import = reset.scss 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Router from './Router';
 import ReactDOM from 'react-dom';
+import './styles/reset.scss';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
