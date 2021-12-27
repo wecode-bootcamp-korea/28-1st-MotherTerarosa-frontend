b3c6783599b25acba0b56099d20f600c9ea6834b
YunKukPark <a01043340999@gmail.com>
2021-12-27 23:42:27 +0900

    Add: 기초 2Dep카테고리 토글 구현 - more 버튼 클릭 시 2Dep 카테고리 토글링

--- src/pages/ShopList/ShopAside.jsx
+++ src/pages/ShopList/ShopAside.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function ShopAside(props) {
+  const [isClickedMore, setIsClickedMore] = useState(false);
   return (
     <aside className="shopListAside">
       <h3 className="shopListTitle">SHOP</h3>
@@ -10,22 +11,31 @@ function ShopAside(props) {
           <Link to="#">연말 선물</Link>
         </li>
         <li className="categoryItem">
-          <Link to="#">커피</Link>
-          <i class="fas fa-plus" />
-          <ul className="categoryList2Dep">
-            <li>
-              <Link to="#">싱글오리진</Link>
-            </li>
-            <li>
-              <Link to="#">블렌드</Link>
-            </li>
-            <li>
-              <Link to="#">드립백</Link>
-            </li>
-            <li>
-              <Link to="#">생두</Link>
-            </li>
-          </ul>
+          <div className="categoryContent">
+            <Link to="#">커피</Link>
+            <i
+              className={`fas ${isClickedMore ? 'fa-minus' : 'fa-plus'} `}
+              onClick={() => {
+                setIsClickedMore(!isClickedMore);
+              }}
+            />
+          </div>
+          {isClickedMore && (
+            <ul className="categoryList2Dep">
+              <li>
+                <Link to="#">싱글오리진</Link>
+              </li>
+              <li>
+                <Link to="#">블렌드</Link>
+              </li>
+              <li>
+                <Link to="#">드립백</Link>
+              </li>
+              <li>
+                <Link to="#">생두</Link>
+              </li>
+            </ul>
+          )}
         </li>
         <li className="categoryItem">
           <Link to="#">식품</Link>
