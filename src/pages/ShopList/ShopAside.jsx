0ff338b3633ba5f3bccebc6299c9cae6077ec507
YunKukPark <a01043340999@gmail.com>
2021-12-28 10:40:33 +0900

    Add: 아코디언 메뉴 transition 추가

--- src/pages/ShopList/ShopAside.jsx
+++ src/pages/ShopList/ShopAside.jsx
@@ -20,22 +20,20 @@ function ShopAside(props) {
               }}
             />
           </div>
-          {isClickedMore && (
-            <ul className="categoryList2Dep">
-              <li>
-                <Link to="#">싱글오리진</Link>
-              </li>
-              <li>
-                <Link to="#">블렌드</Link>
-              </li>
-              <li>
-                <Link to="#">드립백</Link>
-              </li>
-              <li>
-                <Link to="#">생두</Link>
-              </li>
-            </ul>
-          )}
+          <ul className={`categoryList2Dep ${isClickedMore ? 'isActive' : ''} `}>
+            <li>
+              <Link to="#">싱글오리진</Link>
+            </li>
+            <li>
+              <Link to="#">블렌드</Link>
+            </li>
+            <li>
+              <Link to="#">드립백</Link>
+            </li>
+            <li>
+              <Link to="#">생두</Link>
+            </li>
+          </ul>
         </li>
         <li className="categoryItem">
           <Link to="#">식품</Link>
