a472b5539b8b30b5251054a11731b7753f3904d0
YOONKY <lov012726@gmail.com>
2021-12-28 10:34:17 +0900

    [ADD] 글로벌네브 구현. 메인(/), 샵(/shoplist/:category), 로긴(/login) 연동

--- src/components/GlobalNav/GlobalNav.jsx
+++ src/components/GlobalNav/GlobalNav.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './GlobalNav.scss';
 
-function GlobalNav(props) {
-  return <div>NavBar</div>;
+function GlobalNav() {
+  const [scrollPosition, setScrolPosition] = useState(0);
+  const updateScrollPosition = () => {
+    setScrolPosition(window.scrollY || document.documentElement.scrollTop);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', updateScrollPosition);
+  });
+  return (
+    <header className="GlobalNav">
+      <div className={scrollPosition < 1 ? 'originalNav' : 'miniNav'}>
+        <Link to="/">
+          <div className="logoWrapper">
+            <span>MOTHER TERAROSA</span>
+          </div>
+        </Link>
+        <Link to="/shoplist/:category">
+          <div className="categoryWrapper">
+            <span>SHOP</span>
+          </div>
+        </Link>
+        <div className="infoWrapper">
+          <ul className="personalInfoWrapper">
+            <Link to="/login">
+              <li>로그인</li>
+            </Link>
+            <Link to="#">
+              <li>주문/배송</li>
+            </Link>
+            <Link to="#">
+              <li>장바구니</li>
+            </Link>
+            <Link to="#">
+              <li>문의</li>
+            </Link>
+          </ul>
+          <ul className="moreInfoWrapper">
+            <Link to="#">
+              <li>ABOUT</li>
+            </Link>
+            <Link to="#">
+              <li>LOCATIONS</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+      <div className="forJustPageTest" />
+    </header>
+  );
 }
 
 export default GlobalNav;
