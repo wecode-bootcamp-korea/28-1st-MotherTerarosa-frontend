223b8f4d481cc5645ae53428109b7eb1991f2938
YOONKY <lov012726@gmail.com>
2021-12-28 12:54:46 +0900

    [FIX]스크롤 확인용 영역 제거

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Footer.scss';
 
-function Footer(props) {
-  return <div>Footer</div>;
+function Footer() {
+  return (
+    <footer className="Footer">
+      <div className="customerCenter">있을리 있냐</div>
+      <div className="bankInfo">우리 1002 744 000000 입금</div>
+      <div className="footerInfo">ⓒ2021 MOTHERTERAROSA ALL RIGHT RESERVED.</div>
+    </footer>
+  );
 }
 
 export default Footer;
