0b7fceef692737d872aacee810fd8c9362242dd9
Cullen6732 <Cullen6732@gmail.com>
2021-12-28 14:09:48 +0900

    [Add] Sign Up page 구현 중

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-function Login(props) {
-  return <div>여긴 로그인이다.</div>;
-}
+const Login = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="mainLoginContainer">
+          <div className="loginLogo">
+            <h5>LOGIN</h5>
+            <h6>로그인</h6>
+          </div>
+          <div className="innerLoginContainer">
+            <div className="mainLogin">
+              <h6>회원 조회</h6>
+              <input type="text" placeholder="아이디">
+                아이디
+              </input>
+              <input type="password" placeholder="비밀번호">
+                비밀번호
+              </input>
+              <button>로그인</button>
+              <div className="signup" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import './Signup.scss';
 
-function Signup(props) {
-  return <div>여긴 회원가입이다</div>;
-}
+const Signup = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="mainSignUpContainer">
+          <div className="signUpLogo">
+            <h6>Join</h6>
+            <h6>회원가입</h6>
+          </div>
+          <div className="signUpContainer">
+            <div className="signUpInfo">
+              <div className="infoTitle">기본정보 입력</div>
+              <div className="userName">
+                <div className="innerTitle">이름</div>
+                <input type="text" placeholder="이름" />
+              </div>
+              <div className="userId">
+                <div className="innerTitle">아이디</div>
+                <input type="text" placeholder="아이디" />
+              </div>
+              <div className="userPw">
+                <div className="innerTitle">비밀번호</div>
+                <input type="text" placeholder="비밀번호" />
+              </div>
+              <div className="userPwCheck">
+                <div className="innerTitle">비밀번호 확인</div>
+                <input type="text" placeholder="비밀번호 확인" />
+              </div>
+              <button>회원가입</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Signup;
