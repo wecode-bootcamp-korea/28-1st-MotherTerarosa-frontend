8f12c7bc92fd82f303802ce05466bf1cb872dd56
Cullen6732 <Cullen6732@gmail.com>
2021-12-28 21:29:15 +0900

    Sign Up page 구현 완료 & 이벤트 함수 및 조건 구현 중

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,35 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.scss';
 
 const Signup = () => {
+  const [userName, setUserName] = useState('');
+  const [userId, setUserId] = useState('');
+  const [userPw, setUserPw] = useState('');
+  const [userPwCheck, setUserPwCheck] = useState('');
+  const [buttonOn, setButtonOn] = useState(false);
+
+  const handleUserName = event => {
+    setUserName(event.target.value);
+    console.log(setUserName);
+  };
+
+  const handleUserId = event => {
+    setUserId(event.target.value);
+    console.log(setUserId);
+  };
+
+  const handleUserPw = event => {
+    setUserPw(event.target.value);
+    console.log(setUserPw);
+  };
+
+  const handleUserPwCheck = event => {
+    setUserPwCheck(event.target.value);
+    console.log(setUserPwCheck);
+  };
+
+  const isPassedId = () => {
+    return userId.includes('a-z0-9') && 16 > userId.length > 4
+      ? setButtonOn(true)
+      : setButtonOn(false);
+  };
+
   return (
     <section>
       <div className="container">
         <div className="mainSignUpContainer">
           <div className="signUpLogo">
             <h6>Join</h6>
-            <h6>회원가입</h6>
+            <span>회원가입</span>
           </div>
           <div className="signUpContainer">
             <div className="signUpInfo">
               <div className="infoTitle">기본정보 입력</div>
               <div className="userName">
                 <div className="innerTitle">이름</div>
-                <input type="text" placeholder="이름" />
+                <input type="text" onChange={handleUserName} />
               </div>
               <div className="userId">
                 <div className="innerTitle">아이디</div>
-                <input type="text" placeholder="아이디" />
+                <div className="idInner">
+                  <input
+                    type="text"
+                    onChange={handleUserId}
+                    onKeyUp={isPassedId}
+                  />
+                  <span>(영문소문자 / 숫자, 4~16자)</span>
+                </div>
               </div>
               <div className="userPw">
                 <div className="innerTitle">비밀번호</div>
-                <input type="text" placeholder="비밀번호" />
+                <div className="pwInner">
+                  <input type="password" onChange={handleUserPw} />
+                  <span>(특수문자 1가지 이상, 5자 이상)</span>
+                </div>
               </div>
               <div className="userPwCheck">
                 <div className="innerTitle">비밀번호 확인</div>
-                <input type="text" placeholder="비밀번호 확인" />
+                <input type="password" onChange={handleUserPwCheck} />
               </div>
-              <button>회원가입</button>
+              <div className="buttonSetting">
+                <button className={buttonOn ? 'onBtn' : 'offBtn'} type="button">
+                  회원가입
+                </button>
+              </div>
             </div>
           </div>
         </div>
