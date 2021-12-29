fce3cd23027f832c08e80a037f182117520d83ac
YOONKY <lov012726@gmail.com>
2021-12-29 21:59:03 +0900

    [ADD]메인페이지 캐러셀 구현 완료

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import './Main.scss';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
-function Main(props) {
-  return <div>여긴 메인이다.</div>;
+function Main() {
+  const [carouselImg, setCarouselImg] = useState([]);
+  const [currentImg, setCurrentImg] = useState(0);
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data/carouselImageData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setCarouselImg(data.carouselImages);
+      });
+  }, []);
+
+  useEffect(() => {
+    imgRef.current.style.transition = 'all 0.1s linear';
+    imgRef.current.style.transform = `translateX(-${currentImg}00%)`;
+    setTimeout(() => {
+      getNextImg();
+    }, 4000);
+  }, [currentImg]);
+
+  const getPreImg = () => {
+    if (currentImg === 0) return;
+    setCurrentImg(currentImg - 1);
+  };
+
+  const getNextImg = () => {
+    if (currentImg === carouselImg.length - 1) return;
+    setCurrentImg(currentImg + 1);
+  };
+
+  return (
+    <main className="main">
+      <div className="carouselWrapper">
+        <div className="carousel" ref={imgRef}>
+          {carouselImg.map((imgSrc, i) => (
+            <img key={i} alt="img" src={imgSrc} />
+          ))}
+        </div>
+        <div className="arrowWrapper">
+          <MdKeyboardArrowLeft
+            className="arrow"
+            size="40"
+            type="button"
+            onClick={getPreImg}
+          />
+          <MdKeyboardArrowRight
+            className="arrow"
+            size="40"
+            type="button"
+            onClick={getNextImg}
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Main;
