ee7a2e7ad583ef9445c5c9a354b9b0060a6e134f
YOONKY <lov012726@gmail.com>
2021-12-29 15:36:51 +0900

    [FIX]네비게이션, 메인 페이지 삭제

--- src/components/GlobalNav/GlobalNav.jsx
+++ src/components/GlobalNav/GlobalNav.jsx
@@ -1,55 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './GlobalNav.scss';
+import React from 'react';
 
-function GlobalNav() {
-  const [scrollPosition, setScrolPosition] = useState(0);
-  const updateScrollPosition = () => {
-    setScrolPosition(window.scrollY || document.documentElement.scrollTop);
-  };
-  useEffect(() => {
-    window.addEventListener('scroll', updateScrollPosition);
-  });
-  return (
-    <header className="GlobalNav">
-      <div className={scrollPosition < 1 ? 'originalNav' : 'miniNav'}>
-        <Link to="/">
-          <div className="logoWrapper">
-            <span>MOTHER TERAROSA</span>
-          </div>
-        </Link>
-        <Link to="/shoplist/:category">
-          <div className="categoryWrapper">
-            <div>SHOP</div>
-          </div>
-        </Link>
-        <div className="infoWrapper">
-          <ul className="personalInfoWrapper">
-            <Link to="/login">
-              <li className="personalInfo">로그인</li>
-            </Link>
-            <Link to="#">
-              <li className="personalInfo">주문/배송</li>
-            </Link>
-            <Link to="#">
-              <li className="personalInfo">장바구니</li>
-            </Link>
-            <Link to="#">
-              <li className="personalInfo">문의</li>
-            </Link>
-          </ul>
-          <ul className="moreInfoWrapper">
-            <Link to="#">
-              <li className="about">ABOUT</li>
-            </Link>
-            <Link to="#">
-              <li className="location">LOCATIONS</li>
-            </Link>
-          </ul>
-        </div>
-      </div>
-    </header>
-  );
+function GlobalNav(props) {
+  return <div>NavBar</div>;
 }
 
 export default GlobalNav;
