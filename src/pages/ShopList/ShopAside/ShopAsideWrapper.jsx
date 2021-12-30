1c1057375d02cc5e1342b9d36a43f4b573ba4fdd
YunKukPark <a01043340999@gmail.com>
2021-12-30 12:08:00 +0900

    Refactor: Component 분리 ShopAsideWrapper 컴포넌트 추가 - scss 분리 (기존 ShopList.scss 에 모두 묶여있었음) - 기존 main 구조화를 위한 ShopAsideWrapper 컴포넌트로 분리

---
+++ src/pages/ShopList/ShopAside/ShopAsideWrapper.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ShopAside from './ShopAside';
+
+function ShopAsideWrapper({ categories, cateNum }) {
+  return (
+    <aside className="shopListAside">
+      <h3 className="shopListTitle">SHOP</h3>
+      <ul className="categoryList">
+        {categories.map((category) => {
+          const { no: id } = category;
+          return <ShopAside key={id} cateNum={cateNum} {...category} />;
+        })}
+      </ul>
+    </aside>
+  );
+}
+
+export default ShopAsideWrapper;
