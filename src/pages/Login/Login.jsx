60ad151953f9d2df1b52c41b3fda0ff03916f00d
Cullen6732 <Cullen6732@gmail.com>
2022-01-02 02:22:53 +0900

    Add: Login 구현 시작

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import './Login.scss';
 
-function Login(props) {
-  return <div>여긴 로그인이다.</div>;
-}
+const Login = () => {
+  return (
+    <section className="loginMianContiainer">
+      <div className="loginWrapper">
+        <div className="loginLogo">
+          <h1>Login</h1>
+          <h2>로그인</h2>
+        </div>
+        <div className="loginBox">
+          <div className="innerLoginBox">
+            <h3> 회원 조회</h3>
+            <div className="userInfo">
+              <form>
+                <label>
+                  <input type="text" name="id" />
+                </label>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
