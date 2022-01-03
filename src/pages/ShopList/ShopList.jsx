3a6edeff3688013c4bc20765b7e279b37befbac3
YunKukPark <a01043340999@gmail.com>
2022-01-03 18:09:27 +0900

    Remove: never used import 삭제

--- src/pages/ShopList/ShopList.jsx
+++ src/pages/ShopList/ShopList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import ShopAsideWrapper from './ShopAside/ShopAsideWrapper';
 import ProductList from './ProductList/ProductList';
 import Pagination from './Pagination/Pagination';
