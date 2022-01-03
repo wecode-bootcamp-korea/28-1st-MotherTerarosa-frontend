b58bb474d564bd8fc144ca3b35c00fdfddc7185b
Cullen6732 <Cullen6732@gmail.com>
2022-01-03 19:31:28 +0900

    Add: Pull 전 commit

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,28 +8,28 @@ const Login = () => {
         <div className="loginLogo">
           <h1>Login</h1>
           <h2>로그인</h2>
-        </div>
-        <div className="loginBox">
-          <div className="login">
-            <span>회원 조회</span>
-            <div className="userInfo">
-              <form>
-                <label>
-                  <input type="text" name="id" placeholder="아이디" />
-                </label>
-                <label>
-                  <input type="password" name="pw" placeholder="비밀번호" />
-                </label>
-              </form>
-              <button type="button">로그인</button>
-              <div className="SingupPageInfo">
-                <p>아직 회원이 아니신가요?</p>
-                <span>
-                  테라로사에 가입하시면 더 많은 혜택을 누리실 수 있습니다.
-                </span>
-                <button type="button" id="signUp">
-                  신규회원가입
-                </button>
+          <div className="loginBox">
+            <div className="login">
+              <span>회원 조회</span>
+              <div className="userInfo">
+                <form>
+                  <label>
+                    <input type="text" name="id" placeholder="아이디" />
+                  </label>
+                  <label>
+                    <input type="password" name="pw" placeholder="비밀번호" />
+                  </label>
+                </form>
+                <button type="button">로그인</button>
+                <div className="SingupPageInfo">
+                  <p>아직 회원이 아니신가요?</p>
+                  <span>
+                    테라로사에 가입하시면 더 많은 혜택을 누리실 수 있습니다.
+                  </span>
+                  <button type="button" id="signUp">
+                    신규회원가입
+                  </button>
+                </div>
               </div>
             </div>
           </div>
