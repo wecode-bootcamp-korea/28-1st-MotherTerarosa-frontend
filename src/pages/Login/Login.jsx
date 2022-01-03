c504356be1a93eb078f3d252d4e309cb442e3eba
Cullen6732 <Cullen6732@gmail.com>
2022-01-04 00:01:00 +0900

    Add: Login Page 구현 중

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
           <h2>로그인</h2>
           <div className="loginBox">
             <div className="login">
-              <span>회원 조회</span>
+              <span className="loginSpan">회원 조회</span>
               <div className="userInfo">
                 <form>
                   <label>
@@ -18,13 +18,17 @@ const Login = () => {
                     <input type="password" name="pw" placeholder="비밀번호" />
                   </label>
                 </form>
-                <button type="button">로그인</button>
-                <div className="SingupPageInfo">
-                  <p>아직 회원이 아니신가요?</p>
-                  <span>
-                    테라로사에 가입하시면 더 많은 혜택을 누리실 수 있습니다.
-                  </span>
-                  <button type="button" id="signUp">
+                <button className="loginBtn" type="button">
+                  로그인
+                </button>
+                <div className="signUpPageInfo">
+                  <p>
+                    아직 회원이 아니신가요?
+                    <span className="signUpSpan">
+                      테라로사에 가입하시면 더 많은 혜택을 누리실 수 있습니다.
+                    </span>
+                  </p>
+                  <button className="signUpBtn" type="button" id="signUp">
                     신규회원가입
                   </button>
                 </div>
