cc2b8da4f89f8db8389368cc7f3b15f3f0b98486
YunKukPark <a01043340999@gmail.com>
2022-01-04 18:40:02 +0900

    WIP : 통신 확인 중

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'http://10.58.4.111:8000';
 
 export const api = {
-  categories: BASE_URL + '/categories.json',
+  categories: 'http://localhost:3000/data/categories.json',
   products: BASE_URL + '/products',
 };
