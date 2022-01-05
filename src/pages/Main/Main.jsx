99104f536e867c3ff03824889ae585030fd5ce8d
YOONKY <lov012726@gmail.com>
2022-01-05 18:20:20 +0900

    WIP

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,59 +1,63 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { api } from 'config.js';
 import ProductsList from './ProductsList/ProductsList';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
-import { api } from 'config.js';
 import './Main.scss';
 
 function Main() {
-  const [carouselImg, setCarouselImg] = useState([]);
   const [currentImg, setCurrentImg] = useState(0);
   const [productsList, setProductsList] = useState([]);
   const imgRef = useRef(null);
+  // const timer = 1;
 
+  // 데이터 와라
   // useEffect(() => {
   //   fetch(api.main)
   //     .then(res => res.json())
   //     .then(result => {
   //       setProductsList(result.result);
+  //       console.log(result.result);
   //     });
   // }, []);
-  useEffect(() => {
-    fetch('data/data.json')
-      .then(res => res.json())
-      .then(data => {
-        setProductsList(data.productsLists);
-      });
-  }, []);
 
   useEffect(() => {
     imgRef.current.style.transform = `translateX(-${currentImg}00%)`;
-    const handleImage = () => {
-      getNextImg();
-    };
-    setTimeout(handleImage, 4000);
-    return () => {
-      clearTimeout(handleImage);
-    };
+  }, [currentImg]);
+
+  useEffect(() => {
+    const getNextImgByTimer = () => getNextImg();
+    setTimeout(getNextImgByTimer, 1000);
+    console.log(currentImg);
   }, [currentImg]);
 
   const getPreImg = () => {
     if (currentImg === 0) return;
-    setCurrentImg(currentImg - 1);
+    setCurrentImg(currentImg => currentImg - 1);
   };
 
   const getNextImg = () => {
-    if (currentImg === carouselImg.length - 1) setCurrentImg(0);
-    if (currentImg === carouselImg.length - 1) return;
-    setCurrentImg(currentImg + 1);
+    if (currentImg >= 3) return;
+    setCurrentImg(currentImg => currentImg + 1);
   };
 
   return (
     <main className="main">
       <div className="carouselWrapper">
         <div className="carousel" ref={imgRef}>
-          {carouselImg.map((imgUrl, i) => (
-            <img key={i} alt="img" src={imgUrl} />
-          ))}
+          {/* 해당부분 링크는 열어주시는 분 아이피로 수정 요함 */}
+          {/* <Link to="http://10.58.7.78:8000/products/productdetail/3"> */}
+          <img alt="1" src="images/carousel/01.jpg" />
+          {/* </Link> */}
+          {/* <Link to="http://10.58.7.78:8000/products/productdetail/4"> */}
+          <img alt="2" src="images/carousel/02.jpg" />
+          {/* </Link> */}
+          {/* <Link to="http://10.58.7.78:8000/products/productdetail/12"> */}
+          <img alt="3" src="images/carousel/03.jpg" />
+          {/* </Link> */}
+          {/* <Link to="http://10.58.7.78:8000/products/productdetail/14"> */}
+          <img alt="5" src="images/carousel/05.jpeg" />
+          {/* </Link> */}
         </div>
         <div className="arrowWrapper">
           <MdKeyboardArrowLeft
--- src/pages/Main/ProductsList/ProductCard/ProductCard.jsx
+++ src/pages/Main/ProductsList/ProductCard/ProductCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ProductCard.scss';
 
 export default function ProductCard({ products }) {
   return (
     <div className="productCard">
-      <img
-        className="productImg"
-        src={products.image_url}
-        alt={products.name}
-      />
+      <Link to={`/productdetail/:${products.id}`}>
+        <img
+          className="productImg"
+          src={products.image_url}
+          alt={products.name}
+        />
+      </Link>
+
       <div className="productName">{products.name}</div>
       <div className="productDescription">{products.description}</div>
       <div className="tasting_note">{products.tasting_note}</div>
