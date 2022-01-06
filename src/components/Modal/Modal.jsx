860f9d5f651f5e78b4ee9d0f22127e3abe6e62b5
YunKukPark <a01043340999@gmail.com>
2022-01-06 14:57:37 +0900

    Modify: Modal Layout 과 Modal Content 컴포넌트 분리

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,10 @@
 import React from 'react';
-import { printNumberWithComma } from 'utils/printNumberWithComma';
 import './Modal.scss';
 
-function Modal(props) {
-  const { closeModal, quantity, name, image_url: image, price } = props;
+function Modal({ children }) {
   return (
     <div className="Modal">
-      <div className="modalContent">
-        <strong className="modalTitle">결제완료</strong>
-        <div className="modalInfo">
-          <div className="imageWrapper">
-            <img src={image[0]} alt={`${name} 상품이미지`} />
-          </div>
-          <div className="description">
-            <strong className="descriptionTitle">{name}</strong>
-            <p className="quantity">{quantity}개</p>
-            <p className="price">{printNumberWithComma(price * quantity)}원</p>
-          </div>
-        </div>
-        <p className="userInfo">$userName 님 $point원 남았습니다.</p>
-        <div className="confirm">
-          <button className="confirmButton" type="button" onClick={closeModal}>
-            확인
-          </button>
-        </div>
-      </div>
+      <div className="modalContent">{children}</div>
     </div>
   );
 }
